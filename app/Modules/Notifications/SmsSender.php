2a66d7c2e9f8a9ddba00bdbbf71a1fa9643d242f
MoodyBlues04 <105989507+MoodyBlues04@users.noreply.github.com>
2024-07-12 01:16:34 +0300

    small-fix

--- app/Modules/Notifications/SmsSender.php
+++ app/Modules/Notifications/SmsSender.php
@@ -9,6 +9,9 @@ class SmsSender implements Sender
 
     public function send(User $recipient, string $subject, string $content): void
     {
-        // TODO: Implement send() method.
+        if (null === $recipient->phone) {
+            throw new \LogicException("You cannot send via sms, when your phone unset");
+        }
+        throw new \Exception('I have no money to pay for sms api');
     }
 }
